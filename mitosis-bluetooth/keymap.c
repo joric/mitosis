4dca917744302fb611fbfd56300d50cc0b0e9980
joric <joric@users.noreply.github.com>
2018-07-27 01:00:56 +0500

    better layout

--- mitosis-bluetooth/keymap.c
+++ mitosis-bluetooth/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_A,       KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,        KC_SCLN },
   {KC_Z,       KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,      KC_SLSH },
   {XXXXXXX,    KC_LCTL, KC_ESC,  KC_TAB,  KC_QUOT,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,    XXXXXXX },
-  {XXXXXXX,    KC_LGUI, KC_LALT, KC_SPC,  KC_LSFT,        DUAL_R,  KC_BSPC, DUAL_L,  KC_RGUI,     XXXXXXX }
+  {XXXXXXX,    KC_LALT, KC_LGUI, KC_SPC,  KC_LSFT,        DUAL_R,  KC_BSPC, DUAL_L,  KC_RGUI,     XXXXXXX }
 },
 
 
