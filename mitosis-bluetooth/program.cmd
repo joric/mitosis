45140cb08a66c0b5557e8bde5db1682b3d5882b1
joric <joric@users.noreply.github.com>
2018-07-27 05:33:23 +0500

    updated scripts

--- mitosis-bluetooth/program.cmd
+++ mitosis-bluetooth/program.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+set build=Debug
 set option=bluepill
 
 goto %option%
@@ -10,7 +11,7 @@ set openocd=C:\SDK\openocd-0.10.0-dev-00247-g73b676c\bin-x64
 set path=%openocd%;%path%
 
 ::set file=%~dp0..\..\components\softdevice\s130\hex\s130_nrf51_2.0.0_softdevice.hex
-set file=%~dp0mitosis\s130\iar\Release\Exe\ble_app_hids_keyboard_s130_custom.hex
+set file=%~dp0mitosis\s130\iar\%build%\Exe\ble_app_hids_keyboard_s130_custom.hex
 
 openocd -f interface/stlink-v2.cfg -f target/nrf51.cfg ^
 -c init -c "reset halt" -c "flash write_image erase %file:\=/%" -c reset -c exit
@@ -30,7 +31,7 @@ set nordic=C:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin
 set path=%nordic%;%path%
 
 set s130=%~dp0..\..\components\softdevice\s130\hex\s130_nrf51_2.0.0_softdevice.hex
-set file=%~dp0mitosis\s130\iar\Debug\Exe\ble_app_hids_keyboard_s130_custom.hex
+set file=%~dp0mitosis\s130\iar\%build%\Exe\ble_app_hids_keyboard_s130_custom.hex
 
 echo Merging files...
 
