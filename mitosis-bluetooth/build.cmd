40cd73e6c913dde9a6ed91617cb40179b579bf07
joric <joric@users.noreply.github.com>
2018-08-01 13:03:07 +0500

    updated scripts

--- mitosis-bluetooth/build.cmd
+++ mitosis-bluetooth/build.cmd
@@ -1,29 +1,34 @@
 @echo off
 set iar=C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin
-set path=%iar%;%path%
-
 set nordic=C:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin
-set path=%nordic%;%path%
+set path=%iar%;%nordic%;%path%
 
-set file=custom\armgcc\_build\nrf51822_xxac.hex
 set s130=..\..\components\softdevice\s130\hex\s130_nrf51_2.0.0_softdevice.hex
 set dest=..\precompiled_iar\mitosis-bt.hex
 
-if "%1"=="Release" goto :release
+set build=Debug
+if "%1"=="Release" set build=%1
 
-IarBuild.exe custom\iar\mitosis_bluetooth.ewp Debug
+set option=0
+
+if "%option%"=="0" goto iar
+if "%option%"=="1" goto gcc
 
 goto end
 
-:release
+:iar
+set file=custom\iar\%build%\Exe\nrf51822_xxac.hex
+IarBuild.exe custom\iar\mitosis_bluetooth.ewp %build%
+goto publish
 
-::cd custom\armgcc && bash -c make && cd /d %~dp0 || exit
+:gcc
+set file=custom\armgcc\_build\nrf51822_xxac.hex
+cd custom\armgcc && bash -c make && cd /d %~dp0 || exit
 
-set file=custom\iar\Release\Exe\nrf51822_xxac.hex
-IarBuild.exe custom\iar\mitosis_bluetooth.ewp Release
+:publish
+if "%build%"=="Debug" goto end
 
 echo Merging %s130% and %file% to %dest%...
 mergehex.exe --quiet -m %s130% %file% -o %dest%
-::copy /Y %file% %dest%
 
 :end
--- mitosis-bluetooth/program.cmd
+++ mitosis-bluetooth/program.cmd
@@ -1,38 +1,33 @@
-@echo off
+@echo on
 
 set build=Debug
 
-if "%1"=="Release" set build=Release
-
-set merge=0
-
+if "%1"=="" (
 set file=%~dp0custom\iar\%build%\Exe\nrf51822_xxac.hex
-set s130=%~dp0..\..\components\softdevice\s130\hex\s130_nrf51_2.0.0_softdevice.hex
-
 ::GCC version
 ::set file=%~dp0custom\armgcc\_build\nrf51822_xxac.hex
+) else (
+set file=%1
+)
 
-set option=bluepill
+set option=1
 
-goto %option%
+if "%option%"=="0" goto stlinkv2
+if "%option%"=="1" goto blackmagic
 
-:stlink
+:stlinkv2
 
 rem ST-Link V2 and OpenOCD
 
 set openocd=C:\SDK\openocd-0.10.0-dev-00247-g73b676c\bin-x64
 set path=%openocd%;%path%
 
-::set file=%s130%
-
 openocd -f interface/stlink-v2.cfg -f target/nrf51.cfg ^
 -c init -c "reset halt" -c "flash write_image erase %file:\=/%" -c reset -c exit
 
 goto end
 
-:bluepill
-
-rem Blackmagic STM32-based board (Bluepill)
+:blackmagic
 
 set port=COM5
 
@@ -42,19 +37,10 @@ set path=%eabi%;%path%
 set nordic=C:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin
 set path=%nordic%;%path%
 
-set opt=
-if "%merge%"=="1" (
-echo Merging softdevice...
-mergehex.exe --quiet -m %s130% %file% -o out.hex || exit
-set opt=-ex "mon erase"
-) else (
-copy /Y %file% out.hex
-)
-
 echo Uploading...
 
 arm-none-eabi-gdb.exe --quiet --batch -ex "target extended-remote \\.\%port%" -ex "mon swdp_scan" ^
--ex "file out.hex" -ex "att 1" %opt% -ex "load" -ex "run"
+-ex "file %file:\=/%" -ex "att 1" %opt% -ex "load" -ex "run"
 
 :end
 
