78f01479ae5e7917de8713b63ecd9c685bcf9110
joric <joric@users.noreply.github.com>
2018-08-09 05:35:42 +0500

    codestyle fixes

--- mitosis-bluetooth/mitosis_ble_gzll.h
+++ mitosis-bluetooth/mitosis_ble_gzll.h
@@ -489,16 +489,18 @@ char get_debug_cmd(void)
 uint8_t get_battery_level(void)
 {
     // Configure ADC
-    NRF_ADC->CONFIG = (ADC_CONFIG_RES_8bit << ADC_CONFIG_RES_Pos) | (ADC_CONFIG_INPSEL_SupplyOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos) | (ADC_CONFIG_REFSEL_VBG << ADC_CONFIG_REFSEL_Pos) | (ADC_CONFIG_PSEL_Disabled << ADC_CONFIG_PSEL_Pos) | (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos);
+    NRF_ADC->CONFIG = (ADC_CONFIG_RES_8bit << ADC_CONFIG_RES_Pos)
+                    | (ADC_CONFIG_INPSEL_SupplyOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos)
+                    | (ADC_CONFIG_REFSEL_VBG << ADC_CONFIG_REFSEL_Pos)
+                    | (ADC_CONFIG_PSEL_Disabled << ADC_CONFIG_PSEL_Pos)
+                    | (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos);
 
     NRF_ADC->EVENTS_END = 0;
     NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Enabled;
     NRF_ADC->EVENTS_END = 0;    // Stop any running conversions.
     NRF_ADC->TASKS_START = 1;
 
-    while (!NRF_ADC->EVENTS_END)
-    {
-    }
+    while (!NRF_ADC->EVENTS_END);
 
     uint16_t vbg_in_mv = 1200;
     uint8_t adc_max = 255;
